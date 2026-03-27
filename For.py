d66c8e9cf4149315f5e6b3809aa523d3648acab0
HenriMenonMartins <henridaniel.menon.m@gmail.com>
2026-03-27 20:44:04 +0100

    Add files via upload

---
+++ For.py
@@ -0,0 +1,18 @@
+
+obst = ["apfel", "brine", "wassermelone"]
+
+for Gesundes_Essen in obst:
+    print("Die essen sind gesund", Gesundes_Essen)
+
+
+
+Pakete = [12, 25, 18, 30, 8]
+
+for pakete_LISTE in Pakete:
+    print(Pakete)
+
+if pakete_LISTE > 20:
+    print("Pakete schwer")
+
+else:
+    print("Normal")
